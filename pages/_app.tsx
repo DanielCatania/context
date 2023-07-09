82cd7dd4f02b5a2987e36d9437df12dc730e1598
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-07-09 13:44:55 -0300

    adding app context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppProps } from "next/app";
+import AppContext from "@/AppContext";
+import languagesObject from "@/language";
+import { Languages } from "@/language/type";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [languageSelected, setLanguageSelected] = useState<Languages>("pt-br");
+
+  return (
+    <AppContext.Provider
+      value={{
+        languageContext: {
+          state: {
+            language: languagesObject[languageSelected],
+            languageSelected,
+          },
+          setLanguageSelected,
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
 }
